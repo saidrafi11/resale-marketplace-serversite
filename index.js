041e61b75f9619cc17b4d2491b5e8fdc481bf638
saidrafi11 <said.rafi@outlook.com>
2022-11-28 12:07:33 +0600

    done

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
- 
+function verifyJWT(req, res, next) {
+
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message:'auth header not found'})
+  if (!authHeader) {
+    return res.status(401).send({ message: 'auth header not found' })
   }
 
   const token = authHeader.split(' ')[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
-    if(err){
-      res.status(403).send({message: 'jwt verification can not done'})
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      res.status(403).send({ message: 'jwt verification can not done' })
     }
     req.decoded = decoded;
     next();
@@ -45,60 +45,65 @@ async function run() {
     const advertisements = client.db('wamp-db').collection('advertisements')
     const blogs = client.db('wamp-db').collection('blog')
 
-app.get('/isavailable', async(req, res )=> {
+    app.get('/isavailable', async (req, res) => {
 
-})
-
-    app.get('/jwt', async(req, res)=>{
-      const email = req.query.email;
-      const query = {email: email}
-      const user = await allUsers.findOne(query)
-      if(user){
-        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1d'})
-        return res.send({accessToken: token})
-      }
-      
-      res.status(403).send({message: "user email not found"})
     })
 
+    // jwt api
 
-    app.get('/my-orders',verifyJWT, async (req, res) => {
-     
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const user = await allUsers.findOne(query)
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+        return res.send({ accessToken: token })
+      }
+
+      res.status(403).send({ message: "user email not found" })
+    })
+
+    // myorders api
+
+    app.get('/my-orders', verifyJWT, async (req, res) => {
+
       const email = req.query.email
       const decodedEmail = req.decoded.email
       if (email !== decodedEmail) {
-        return res.status(403).send({message: 'decoded email not found'})
-        
+        return res.status(403).send({ message: 'decoded email not found' })
+
       }
-      const query = {email: email}
+      const query = { email: email }
       const myorders = await allBookings.find(query).toArray()
       res.send(myorders)
     })
 
+    // available product api
+
 
     app.get('/available', async (req, res) => {
-     
+
       const productId = req.query.id
       // const decodedEmail = req.decoded.email
       // if (email !== decodedEmail) {
       //   return res.status(403).send({message: 'decoded email not found'})
-        
+
       // }
-      const query = {product_id: productId}
+      const query = { product_id: productId }
       const bookedProduct = await allBookings.find(query).toArray()
       // console.log(bookedProduct);
       res.send(bookedProduct)
     })
 
-    
-  
+
+
 
     app.put('/allproducts/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: ObjectId(id)}
+      const filter = { _id: ObjectId(id) }
       const update = req.body;
       console.log(update);
-      const option = {upsert: true};
+      const option = { upsert: true };
       const edited = {
         $set: {
           available: update.available
@@ -108,24 +113,29 @@ app.get('/isavailable', async(req, res )=> {
       res.send(result)
     })
 
-    
+
+    // all product API
+
+
     app.get('/allproducts/:id', async (req, res) => {
       const id = req.query.id;
-      const query = {_id: ObjectId(id)}
-      
+      const query = { _id: ObjectId(id) }
+
       console.log(update);
-      
+
       const result = await productCollection.find(query).toArray();
       res.send(result)
     })
 
 
+
+    // add user API
     app.put('/allusers/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: ObjectId(id)}
+      const filter = { _id: ObjectId(id) }
       const update = req.body;
       console.log(update);
-      const option = {upsert: true};
+      const option = { upsert: true };
       const edited = {
         $set: {
           isVerifiedSeller: update.isVerifiedSeller
@@ -137,6 +147,9 @@ app.get('/isavailable', async(req, res )=> {
     })
 
 
+
+    // my products API
+
     app.get('/my-products', async (req, res) => {
       let query = {}
       const email = req.query.email
@@ -144,7 +157,7 @@ app.get('/isavailable', async(req, res )=> {
         query = {
 
           sellerEmail: email
-            
+
         }
       }
       const myproducts = await productCollection.find(query).toArray()
@@ -152,63 +165,71 @@ app.get('/isavailable', async(req, res )=> {
     })
 
 
+
+    // All seller API
+
     app.get('/allsellers', async (req, res) => {
       let query = {}
-      
-     
-        query = {
 
-          role: 'seller'
-            
-        }
-      
+
+      query = {
+
+        role: 'seller'
+
+      }
+
       const myproducts = await allUsers.find(query).toArray()
       res.send(myproducts)
     })
 
+    // Is verified seller API
 
     app.get('/verifiedseller', async (req, res) => {
       let query = {}
       const email = req.query.email
-     
-        query = {email: email}
-      
+
+      query = { email: email }
+
       const verifiedSeller = await allUsers.find(query).toArray()
       res.send(verifiedSeller)
     })
 
 
+    // All user Api
 
     app.get('/allusers', async (req, res) => {
       let query = {}
-      
-     
-        query = {
 
-          role: 'user'
-            
-        }
-      
+
+      query = {
+
+        role: 'user'
+
+      }
+
       const myproducts = await allUsers.find(query).toArray()
       res.send(myproducts)
     })
+
+    // specific product api
 
 
     app.get('/product', async (req, res) => {
       let query = {}
       const id = req.query.id
-     
-        query = {
 
-          _id: ObjectId(id)
-            
-        }
-      
+      query = {
+
+        _id: ObjectId(id)
+
+      }
+
       const myproduct = await productCollection.find(query)
       res.send(myproduct)
     })
 
 
+    // Get orders API
 
     app.get('/orders', async (req, res) => {
 
@@ -220,7 +241,7 @@ app.get('/isavailable', async(req, res )=> {
       res.send(orders)
     })
 
-
+    // Blog API
 
     app.get('/blog', async (req, res) => {
 
@@ -232,6 +253,7 @@ app.get('/isavailable', async(req, res )=> {
       res.send(orders)
     })
 
+    // Load category wise products
 
     app.get('/category', async (req, res) => {
       const query = {}
@@ -240,6 +262,8 @@ app.get('/isavailable', async(req, res )=> {
       res.send(products)
 
     });
+
+    // Insert user Api
 
 
     app.post('/insertuser', (req, res) => {
@@ -250,6 +274,7 @@ app.get('/isavailable', async(req, res )=> {
 
 
 
+    // Insert products 
 
     app.post('/insertproduct', (req, res) => {
       const insertProduct = req.body;
@@ -257,11 +282,19 @@ app.get('/isavailable', async(req, res )=> {
       res.send(result)
     })
 
+
+    //  Advertise product insert 
+    
+    
     app.post('/advertise', (req, res) => {
       const insertProduct = req.body;
       const result = advertisements.insertOne(insertProduct);
       res.send(result)
     })
+
+
+    // Get advertised product 
+
 
     app.get('/advertise', async (req, res) => {
 
@@ -274,7 +307,7 @@ app.get('/isavailable', async(req, res )=> {
     })
 
 
-
+    // Get all bookings
 
     app.post('/allbookings', (req, res) => {
       const bookingInfo = req.body;
@@ -282,7 +315,7 @@ app.get('/isavailable', async(req, res )=> {
       res.send(result)
     })
 
-
+// Get product by category id
 
     app.get('/category/:id', async (req, res) => {
       const id = req.params.id;
@@ -304,31 +337,48 @@ app.get('/isavailable', async(req, res )=> {
     //   res.send()
     // })
 
+// Delete seller 
 
-    app.delete('/sellers/:id', async(req, res)=> {
+
+    app.delete('/sellers/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await allUsers.deleteOne(query);
       console.log(result);
-    res.send(result)
+      res.send(result)
     })
 
+// Delete user api
 
-
-    app.delete('/users/:id', async(req, res)=> {
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await allUsers.deleteOne(query);
       console.log(result);
-    res.send(result)
+      res.send(result)
     })
 
-    app.get('/users/seller/:email', async(req, res)=> {
+    // Check role seller
+
+    app.get('/users/seller/:email', async (req, res) => {
       const email = req.params.email
-      const query = {email}
+      const query = { email }
       const user = await allUsers.findOne(query);
       // console.log(user);
-    res.send({isSeller: user?.role === 'seller'})
+      res.send({ isSeller: user?.role === 'seller' })
+    })
+
+
+    // check role admin 
+
+
+    
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email }
+      const user = await allUsers.findOne(query);
+      // console.log(user);
+      res.send({ isAdmin: user?.role === 'admin' })
     })
 
 
